2573667aaafea2c1ae21915d96861c89aba50cbe
TBD <weinanshao@163.com>
2020-03-06 14:27:22 +0800

    remove temp

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -6,7 +6,6 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-tempTheta = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
