159ee6cbef6d6c9d85cc5d2463f3ba4f3669611c
TBD <weinanshao@163.com>
2020-03-08 15:43:25 +0800

    ex2 Evaluating logistic regression

--- Exam/ex2/predict.m
+++ Exam/ex2/predict.m
@@ -15,10 +15,9 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
+z = X * theta;
+h = sigmoid(z);
+p = h >= 0.5;
 
 
 % =========================================================================
