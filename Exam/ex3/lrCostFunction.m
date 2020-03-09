0ec542c92ccb70f8378bd83a5bcaeac7d7d78e40
TBD <weinanshao@163.com>
2020-03-09 16:38:55 +0800

    ex3: 1.3.3 Vectorizing regularized logistic regression

--- Exam/ex3/lrCostFunction.m
+++ Exam/ex3/lrCostFunction.m
@@ -39,13 +39,11 @@ grad = zeros(size(theta));
 % cost
 z = X * theta;
 h = sigmoid(z);
-J = 1 / m * sum((-y)' * log(h) - (1 .- y)' * log(1 .- h));
+J = 1 / m * sum((-y)' * log(h) - (1 .- y)' * log(1 .- h)) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
 
 % grad
-grad = 1 / m .* X' * (h - y);
+grad = 1 / m .* X' * (h - y) + (lambda / m) .* theta .* [0; ones(length(theta)-1, 1)];
 
 % =============================================================
-
-grad = grad(:);
 
 end
