fe94e1b154085aa612c70bd32bc649ed5bca313d
TBD <weinanshao@163.com>
2020-03-10 22:02:44 +0800

    ex4: 2.5 Regularized Neural Networks

--- Exam/ex4/nnCostFunction.m
+++ Exam/ex4/nnCostFunction.m
@@ -108,12 +108,9 @@ endfor
 Theta1_grad = 1 / m .* Theta1_grad;
 Theta2_grad = 1 / m .* Theta2_grad;
 
-
-
-
-
-
-
+% Regularized gradients
+Theta1_grad = Theta1_grad + lambda / m .* [zeros(size(Theta1,1), 1), Theta1(:,(2:end))];
+Theta2_grad = Theta2_grad + lambda / m .* [zeros(size(Theta2,1), 1), Theta2(:,(2:end))];
 
 
 % -------------------------------------------------------------
