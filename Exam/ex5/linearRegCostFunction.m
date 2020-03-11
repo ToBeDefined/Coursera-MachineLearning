9149cf881b887419809eba4133fc604c68d845cf
TBD <weinanshao@163.com>
2020-03-11 15:55:22 +0800

    ex5: 1.3 Regularized linear regression gradient

--- Exam/ex5/linearRegCostFunction.m
+++ Exam/ex5/linearRegCostFunction.m
@@ -21,7 +21,7 @@ grad = zeros(size(theta));
 
 h = X * theta;
 J = 1 / (2 * m) * sum((h - y) .^ 2) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
-
+grad = 1 / m .* (X' * (h - y)) .+ lambda / m .* [0; theta(2:end)];
 
 % =========================================================================
 
