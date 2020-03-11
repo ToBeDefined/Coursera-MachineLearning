4eae8fc750fa2ec146460df5dfa426d238efc91b
TBD <weinanshao@163.com>
2020-03-11 16:56:39 +0800

    ex5: 3.2 Optional (ungraded) exercise: Adjusting the reg- ularization parameter (test lambda = 1 and 100)

--- Exam/ex5/ex5.m
+++ Exam/ex5/ex5.m
@@ -164,36 +164,37 @@ pause;
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 0;
-[theta] = trainLinearReg(X_poly, y, lambda);
+for lambda = [0, 1, 100];
+    [theta] = trainLinearReg(X_poly, y, lambda);
 
-% Plot training data and fit
-figure(1);
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-plotFit(min(X), max(X), mu, sigma, theta, p);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
-title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
+    % Plot training data and fit
+    figure(1);
+    plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+    plotFit(min(X), max(X), mu, sigma, theta, p);
+    xlabel('Change in water level (x)');
+    ylabel('Water flowing out of the dam (y)');
+    title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
 
-figure(2);
-[error_train, error_val] = ...
-    learningCurve(X_poly, y, X_poly_val, yval, lambda);
-plot(1:m, error_train, 1:m, error_val);
+    figure(2);
+    [error_train, error_val] = ...
+        learningCurve(X_poly, y, X_poly_val, yval, lambda);
+    plot(1:m, error_train, 1:m, error_val);
 
-title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
-xlabel('Number of training examples')
-ylabel('Error')
-axis([0 13 0 100])
-legend('Train', 'Cross Validation')
+    title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
+    xlabel('Number of training examples')
+    ylabel('Error')
+    axis([0 13 0 100])
+    legend('Train', 'Cross Validation')
 
-fprintf('Polynomial Regression (lambda = %f)\n\n', lambda);
-fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
-for i = 1:m
-    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
-end
+    fprintf('Polynomial Regression (lambda = %f)\n\n', lambda);
+    fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+    for i = 1:m
+        fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+    end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+    fprintf('Program paused. Press enter to continue.\n');
+    pause;
+endfor
 
 %% =========== Part 8: Validation for Selecting Lambda =============
 %  You will now implement validationCurve to test various values of 
