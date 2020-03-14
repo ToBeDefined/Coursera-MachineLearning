04abdcaa941988552bc8e30ed47d5a2770c88f70
TBD <weinanshao@163.com>
2020-03-14 19:55:11 +0800

    ex6: 1.1 Example Dataset 1 (try different values of C)

--- Exam/ex6/ex6.m
+++ Exam/ex6/ex6.m
@@ -50,12 +50,15 @@ fprintf('\nTraining Linear SVM ...\n')
 
 % You should try to change the C value below and see how the decision
 % boundary varies (e.g., try C = 1000)
-C = 1;
-model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
-visualizeBoundaryLinear(X, y, model);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% 1.1 Example Dataset 1
+for C = [1, 100, 1000]
+    model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
+    visualizeBoundaryLinear(X, y, model);
+    title(sprintf("C = %d", C));
+    fprintf('Program paused. Press enter to continue.\n');
+    pause;
+endfor
 
 %% =============== Part 3: Implementing Gaussian Kernel ===============
 %  You will now implement the Gaussian kernel to use
