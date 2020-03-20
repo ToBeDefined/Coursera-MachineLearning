55407dd88bf2ee7964e3f862bf1281225501c127
TBD <weinanshao@163.com>
2020-03-20 15:46:30 +0800

    ex6 (2.6) Optimize

--- ProgrammingExercise/ex6/spamassassin/main.m
+++ ProgrammingExercise/ex6/spamassassin/main.m
@@ -4,7 +4,7 @@ clear ; close all; clc
 
 addpath('libsvm');
 
-[X_train, y_train, X_val, y_val, X_test, y_test, vocabList] = getAllData();
+[X_train, y_train, X_val, y_val, X_test, y_test, ~] = getAllData();
 
 clc;
 
@@ -57,7 +57,7 @@ clc;
 
 
 %% ========================= Training with different C ========================= 
-CVec = [0.01; 0.05; 0.1; 0.5; 1; 5; 10; 50; 100; 500; 1000; 5000; 10000];
+CVec = [[0.01:0.02:0.09]'; [0.1:0.2:0.9]'; [1:2:9]';];
 Accuracy = zeros(1:length(CVec), 3);
 Models = {};
 for i = 1:length(CVec)
